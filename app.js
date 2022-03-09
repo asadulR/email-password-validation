13f29f6354dbf51dd6786f63fb54369ab4159109
asadulR <asad1252001@gmail.com>
2022-03-09 07:33:37 +0600

    another js commit

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const checkEmail = () => {
-    alert('ol')
+    const emailField = document.getElementById('inputEmail');
+    const email = emailField.value;
+
+
+    var filter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!filter.test(email)) {
+        alert('Please provide a valid email address');
+        email.focus;
+        return false;
+    }
+    
 }
