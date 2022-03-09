e2df10a7112573c53881269566972131ba962614
asadulR <asad1252001@gmail.com>
2022-03-09 07:48:44 +0600

    email validation commit

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ const checkEmail = () => {
     const emailField = document.getElementById('inputEmail');
     const email = emailField.value;
 
+    const worngError = document.getElementById('worng-email');
+    const rightEmail = document.getElementById('correct-email');
 
     var filter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!filter.test(email)) {
-        alert('Please provide a valid email address');
+        
+        // alert('Please provide a valid email address');
+        worngError.classList.remove('d-none');
+        rightEmail.classList.add('d-none')
+
         email.focus;
         return false;
     }
+    else if(filter.test(email)){
+        rightEmail.classList.remove('d-none')
+        worngError.classList.add('d-none');
+
+    }
     
+    emailField.value = '';
 }
